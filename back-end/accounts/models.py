b9b2059cf4094fc9e389ca1da31118d85d7cd5d3
Abdelazeem777 <abdelazeem263@gmail.com>
2019-11-22 20:31:08 +0200

    registration almost finished

---
+++ back-end/accounts/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator,MinValueValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Create your models here.
+class Profile(models.Model):
+    gender_CHOICES = [
+        ('M','Male'),
+        ('F','Female')
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    phone = models.CharField(max_length=20)
+    gender = models.CharField(
+        max_length=1,
+        choices = gender_CHOICES,
+        default = 'M'
+        )
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    weight = models.SmallIntegerField(
+        default = 10,
+        validators = [MaxValueValidator(300),MinValueValidator(10)]
+    )
+    height = models.SmallIntegerField(
+        default = 10,
+        validators = [MaxValueValidator(300),MinValueValidator(10)]
+    )
+    smoking = models.BooleanField(default = False)
+    
+    @receiver(post_save,sender = User)
+    def create_user_profile(sender, instance,created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+    
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance,created, **kwargs):
+        instance.profile.save()
