4021c230f21301de64f267acc80359ddd5830e4d
Abdulrahmn Ali Hussain <ParrotStone@gmail.com>
2020-02-19 04:15:21 +0200

    Modified old merge tags

--- back-end/accounts/models.py
+++ back-end/accounts/models.py
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-from django.db import models
-from django.contrib.auth.models import User
-from django.core.validators import MaxValueValidator,MinValueValidator
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-# Create your models here.
-class Profile(models.Model):
-    gender_CHOICES = [
-        ('M','Male'),
-        ('F','Female')
-    ]
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    phone = models.CharField(max_length=20)
-    gender = models.CharField(
-        max_length=1,
-        choices = gender_CHOICES,
-        default = 'M'
-        )
-    country = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    weight = models.SmallIntegerField(
-        default = 10,
-        validators = [MaxValueValidator(300),MinValueValidator(10)]
-    )
-    height = models.SmallIntegerField(
-        default = 10,
-        validators = [MaxValueValidator(300),MinValueValidator(10)]
-    )
-    smoking = models.BooleanField(default = False)
-    birthdate = models.DateField(auto_now=False,auto_now_add=False,null=True,blank=True)
-    @receiver(post_save,sender = User)
-    def create_user_profile(sender, instance,created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance)
-    
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance,created, **kwargs):
-        instance.profile.save()
-=======
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator,MinValueValidator
 from django.db import models
@@ -105,4 +63,3 @@ class Profile(models.Model):
         instance.profile.save()
 
     """
->>>>>>> 8d37f99b6568ce3c82fc7453c89d178a8c37cf7a
