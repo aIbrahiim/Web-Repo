88f42b94878ceea5d1d2f67f9e5ced885cf1192a
Ubuntu <tabib123@tabibVM.h3jxb0j3ifpuhlmqwtzipquvqe.bx.internal.cloudapp.net>
2020-02-19 16:55:51 +0000

    Added old files back into the Tabib folder
    Committer: ParrotStone <ParrotStone@gmail.com>

--- back-end/Tabib/wsgi.py
+++ back-end/Tabib/wsgi.py
@@ -1,16 +1,26 @@
 """
-WSGI config for Tabib project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
+exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
-
 import os
+import time
+import traceback
+import signal
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tabib.settings')
+sys.path.append('/home/tabib123/Web-Repo/back-end')
+# adjust the Python version in the line below as needed
 
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Tabib.settings")
+
+try:
+    application = get_wsgi_application()
+except Exception:
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
