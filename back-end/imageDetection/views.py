6d0ada0f53b02f84ff40a0008c13867abcec38e2
Abdo <abdo@abdo.com>
2020-03-03 18:47:47 +0200

    skin detection feature is working with android

--- back-end/imageDetection/views.py
+++ back-end/imageDetection/views.py
@@ -13,6 +13,9 @@ import cv2
 import pandas as pd
 from keras import backend as K
 from rest_framework import permissions
+import base64
+from django.core.files.base import ContentFile
+
 
 mapper = joblib.load('current_model_mapping.pkl')
 mapper = {v:k for k,v in mapper.items()}
@@ -61,22 +64,69 @@ class FileView(APIView):
       return JsonResponse("error", safe=False)
 
 
+class FileViewAndroid(APIView):
+  #permission_classes = [permissions.IsAuthenticated]
+
+  #parser_classes = (MultiPartParser, FormParser)
+  
+  def post(self, request, *args, **kwargs):
+    #file_serializer = FileSerializer(data=request.data)
+    
+    
+    projectPath = os.path.dirname(__file__) #get the current directory of the project
+
+    model = load_model('current_model.h5')
+
+    mapper = joblib.load('current_model_mapping.pkl')
+    
+    mapper = {v:k for k,v in mapper.items()}
+    
+    def get_pred_class_name(pred_class_number):
+      global mapper
+      return mapper[pred_class_number]
+
+    body_unicode = request.body.decode('utf-8')
+    body_data = json.loads(body_unicode)
+    img64 = str(body_data['img'])
+    format, imgstr = img64.split(';base64,')
+    print("format", format)
+    ext = format.split('/')[-1]
+
+    data = base64.b64decode(imgstr) 
+    file_name = "myphoto." + ext
+    path = 'tmpImg/'+file_name
+    newFile = open(path,'wb')
+    newFile.write(data)
+    newFile.close()
+    img = cv2.imread(path)
+    pic = preprocess_single_image(img)
+    pred_class = model.predict_classes(pic)[0]
+    pred_class_name = get_pred_class_name(pred_class)
+
+    if pred_class_name is not None:
+      ans = "Predicted is {}".format(pred_class_name.replace("%20"," "))
+    
+      return JsonResponse({'ans':ans}, safe=False)
+    else:
+      return JsonResponse("error", safe=False)
+ 
+
+    
+
+
+
+
 def preprocess_single_image(pic):
+  pic = cv2.resize(pic, (120,120))
+    
+  pic = pic.astype('float32')
+    
+  pic /= 255
     
     
+  pic = pic.reshape(-1,120,120,3)
     
-    pic = cv2.resize(pic, (120,120))
-    
-    pic = pic.astype('float32')
-    
-    pic /= 255
-    
-    
-    pic = pic.reshape(-1,120,120,3)
-    #print(pic)
-    """
-    """
-    return pic
+  return pic
 
 
 
