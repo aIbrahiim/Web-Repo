ffb32163923d03d11b84da7cdb83ea9ad7b739af
woodyinho <abdoibrahim331@yahoo.com>
2020-03-11 02:41:02 +0200

    Update urls.py

--- back-end/imageDetection/urls.py
+++ back-end/imageDetection/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt import views as jwt_views
-from .views import FileView
+from .views import FileView,FileViewAndroid
 
 
 
 urlpatterns = [
-    path('',FileView.as_view(),name ='refresh')
+    path('',FileView.as_view(),name ='refresh'),
+    path('android/',FileViewAndroid.as_view(),name ='refresh')
 
 ]
