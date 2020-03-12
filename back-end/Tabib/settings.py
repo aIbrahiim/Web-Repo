739507d0254688ad7be9144dbbf4056e9d987d00
woodyinho <abdoibrahim331@yahoo.com>
2020-03-12 15:25:30 +0200

    conflicts solved

--- back-end/Tabib/settings.py
+++ back-end/Tabib/settings.py
@@ -88,11 +88,14 @@ WSGI_APPLICATION = 'Tabib.wsgi.application'
 
 DATABASES = {
    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'TabibDB',
+        'USER' : 'root',
+        'PASSWORD': '1998/9/25',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
