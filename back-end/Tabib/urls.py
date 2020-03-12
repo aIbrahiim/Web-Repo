4d7fdb0f406a65f55ef84f1cdea2141ae626252e
woodyinho <abdoibrahim331@yahoo.com>
2020-03-12 02:50:57 +0200

    Update urls.py

--- back-end/Tabib/urls.py
+++ back-end/Tabib/urls.py
@@ -19,10 +19,11 @@ from django.conf.urls.static import static
 
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/',include('accounts.api.urls')),
-
-]
+    path('test/',include('chatbot.urls')),
+    path('upload/',include('imageDetection.urls'))
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
